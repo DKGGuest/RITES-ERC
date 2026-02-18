9b3536f2b8d44d37f9db495ce054b5b7eefe550a
riyansh-dkg <riyansh@dkgrouplabs.com>
2026-02-18 13:12:38 +0530

    admin button issue

--- src/components/admin-dashboard/Modal.jsx
+++ src/components/admin-dashboard/Modal.jsx
@@ -4,11 +4,11 @@ export const Modal = ({ isOpen, title, children, onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="modal-overlay active">
-            <div className="modal">
-                <div className="modal-header">
-                    <h2 className="modal-title">{title}</h2>
-                    <button className="modal-close" onClick={onClose}>
+        <div className="admin-modal-overlay active">
+            <div className="admin-modal">
+                <div className="admin-modal-header">
+                    <h2 className="admin-modal-title">{title}</h2>
+                    <button className="admin-modal-close" onClick={onClose}>
                         ×
                     </button>
                 </div>
